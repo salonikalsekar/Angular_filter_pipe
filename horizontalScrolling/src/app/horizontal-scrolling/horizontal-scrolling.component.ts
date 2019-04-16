7f6f26d1e82567c9c4541a5a6b15e56d938c010f
Saloni Kalsekar <saloniskalsekar@gmail.com>
2019-04-15 21:58:33 -0600

    horizontal_scrolling

---
+++ horizontalScrolling/src/app/horizontal-scrolling/horizontal-scrolling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-scrolling',
+  templateUrl: './horizontal-scrolling.component.html',
+  styleUrls: ['./horizontal-scrolling.component.css']
+})
+export class HorizontalScrollingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
